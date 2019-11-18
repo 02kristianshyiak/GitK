2bcc1a9e2891d2fa768f4a62e294798e7cf75dcf
Kristian <02kristian.shyiak@gulllakecs.org>
2019-11-18 09:53:51 -0500

    Add some code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
+
+public class StringVariables {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner user_input = new Scanner(System.in);
+
+	String first_name;
+	System.out.print("Enter your first name: ");
+	first_name = user_input.next();
+
+	String family_name;
+	System.out.print("Enter your family name: ");
+	family_name = user_input.next();
+
+	String full_name;
+	full_name = first_name + " " + family_name;
+
+	System.out.println("You are " + full_name);
     }
 }
