ff02659dde7970373deec1ba56bb00b2cff701ee
Kristian <02kristian.shyiak@gulllakecs.org>
2019-11-19 09:40:31 -0500

    Add Coffee Stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,20 @@ public class Main {
 	Scanner user_input = new Scanner(System.in);
 
 	String first_name;
-	System.out.print("Enter your first name: ");
+	System.out.print("What is your name? ");
 	first_name = user_input.next();
 
 	String family_name;
-	System.out.print("Enter your family name: ");
+	System.out.print("What coffee do you want? ");
 	family_name = user_input.next();
+
+	String size;
+	System.out.print("What size do you want? (Large, Medium, Small) ");
+	size = user_input.next();
+
+	String shots;
+	System.out.print("How many shots of expresso do you want? ");
+	shots = user_input.next();
 
 	String full_name;
 	full_name = first_name + " " + family_name;
