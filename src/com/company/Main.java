6211812de1d7790787fac7b85385de97d8424aa9
Kristian <02kristian.shyiak@gulllakecs.org>
2019-12-04 10:24:26 -0500

    Add if statements(prices)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,29 +5,48 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	Scanner user_input = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
 
 	String first_name;
-	System.out.print("What is your first name? ");
-	first_name = user_input.next();
-
-	String last_name;
-	System.out.print("What is your last name? ");
-	last_name = user_input.next();
+	System.out.print("What is your name? ");
+	first_name = sc.nextLine();
 
 	String family_name;
-	System.out.print("What coffee do you want? (Cortado, Affogato, Doppio): ");
-	family_name = user_input.next();
+	System.out.print("What coffee do you want? (Cortado, Affogato, Doppio)(Start with a capital please): ");
+	family_name = sc.nextLine();
 
 	String size;
 	System.out.print("What size do you want? (Large, Medium, Small): ");
-	size = user_input.next();
+	size = sc.nextLine();
 
 	String shots;
-	System.out.print("How many shots of expresso do you want? ");
-	shots = user_input.next();
+	System.out.print("How many shots of espresso do you want? ");
+	shots = sc.nextLine();
 
-	System.out.println("Hello " + first_name + " " + last_name + " we have your " + size + " "+ family_name + " with " + shots + " shots of expresso.");
-	
+	System.out.println("Hello " + first_name + " we have your " + size + " "+ family_name + " with " + shots + " shots of espresso.");
+
+	double original_price = 0;
+
+	if(family_name.equals("Cortado")){
+		original_price = 3.99;
+		System.out.println("That'll be " + original_price);
+	}
+	if(family_name.equals("Affogato")) {
+		original_price = 4.75;
+		System.out.println("That'll be " + original_price);
+	}
+
+	if(family_name.equals("Doppio")) {
+		original_price = 3.25;
+		System.out.println("That'll be " + original_price);
+	}
+
+		if(shots.equals("1")) {
+		System.out.println("plus 1.20 for the shot of espresso");
+		original_price = original_price + 1.20;
+	}
+
+
+	System.out.println(original_price);
     }
 }
