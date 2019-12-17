ed3d08c5ee475bab956bc8b4e5eded4bb466a3f8
Kristian <02kristian.shyiak@gulllakecs.org>
2019-12-17 09:06:39 -0500

    Add times to the coffee

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -39,66 +39,90 @@ public class Main {
 	System.out.println("Hello " + first_name + " we have your " + size + " " + coffee + " with " + shots + " shots of espresso and you said " + cream + " to cream and " + cups + " cups of coffee.");
 
 	double original_price = 0;
-	double estimated_time = 4;
+	double estimated_time = 0;
 	double seconds = 0;
 
 	if(coffee.equals("Cortado") || coffee.equals("cortado")){
 		original_price = 3.99;
+		estimated_time = 3;
+		seconds = 30;
 		System.out.println("That'll be " + original_price);
 	}
 
 	if(coffee.equals("Affogato") || coffee.equals("affogato")) {
 		original_price = 4.75;
+		estimated_time = 5;
+		seconds = 0;
 		System.out.println("That'll be " + original_price);
 	}
 
 	if(coffee.equals("Doppio") || coffee.equals("doppio")) {
 		original_price = 3.25;
+		estimated_time = 1;
+		seconds = 25;
 		System.out.println("That'll be " + original_price);
 	}
 
 		if(coffee.equals("Latte") || coffee.equals("latte")) {
 			original_price = 2.95;
+			estimated_time = 2;
+			seconds = 45;
 			System.out.println("That'll be " + original_price);
 		}
 
 		if(coffee.equals("Irish Coffee") || coffee.equals("irish coffee")) {
 			original_price = 6.99;
+			estimated_time = 5;
+			seconds = 0;
 			System.out.println("That'll be " + original_price);
 		}
 
 		if(coffee.equals("Turkish Coffee") || coffee.equals("turkish coffee")) {
 			original_price = 2.89;
+            estimated_time = 3;
+            seconds = 15;
 			System.out.println("That'll be " + original_price);
 		}
 
 		if(coffee.equals("Caffe Americano") || coffee.equals("caffe americano")) {
 			original_price = 2.25;
+			estimated_time = 3;
+			seconds = 30;
 			System.out.println("That'll be " + original_price);
 		}
 
 		if(coffee.equals("Cappuccino") || coffee.equals("cappuccino")) {
 			original_price = 3.65;
+            estimated_time = 4;
+            seconds = 20;
 			System.out.println("That'll be " + original_price);
 		}
 
 		if(coffee.equals("Caffe Mocha") || coffee.equals("caffe mocha")) {
 			original_price = 4.15;
+            estimated_time = 10;
+            seconds = 0;
 			System.out.println("That'll be " + original_price);
 		}
 
 		if(coffee.equals("Iced Coffee") || coffee.equals("iced coffee")) {
 			original_price = 2.25;
+            estimated_time = 2;
+            seconds = 30;
 			System.out.println("That'll be " + original_price);
 		}
 
 		if(coffee.equals("Espressino") || coffee.equals("espressino")) {
 			original_price = 3.25;
+            estimated_time = 3;
+            seconds = 25;
 			System.out.println("That'll be " + original_price);
 		}
 
 		if(coffee.equals("Eggnog") || coffee.equals("eggnog")) {
 			original_price = 3.50;
+            estimated_time = 10;
+            seconds = 0;
 			System.out.println("That'll be " + original_price);
 		}
 
@@ -152,28 +176,28 @@ public class Main {
 		if(cups.equals("2")) {
 			System.out.println("And you have 2 cups of that.");
 			estimated_time = estimated_time * 2;
-			seconds = seconds + 15;
+			seconds = seconds + 0;
 			original_price = original_price * 2;
 		}
 
 		if(cups.equals("3")) {
 			System.out.println("And you have 3 cups of that.");
 			estimated_time = estimated_time * 3;
-			seconds = seconds + 30;
+			seconds = seconds + 15;
 			original_price = original_price * 3;
 		}
 
 		if(cups.equals("4")) {
 			System.out.println("And you have 4 cups of that.");
 			estimated_time = estimated_time * 4;
-			seconds = seconds + 45;
+			seconds = seconds + 30;
 			original_price = original_price * 4;
 		}
 
 		if(cups.equals("5")) {
 			System.out.println("And you have 5 cups of that.");
 			estimated_time = estimated_time * 5;
-			seconds = seconds + 0;
+			seconds = seconds + 45;
 			original_price = original_price * 5;
 		}
 
